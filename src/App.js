7bc169c6f83ed0f9ec933b331d73385e9800e868
Evgeny Kuzyakov <ek@nearprotocol.com>
2021-08-13 13:25:13 -0700

    Implement wallet logout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import buttonImage from './button.png';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [showTrololo, setShowTrololo] = useState(false);
+
+  const logOut = (e) => {
+    e.preventDefault();
+    setShowTrololo(true);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {showTrololo ? <>
+          <div className="video-container">
+            <iframe
+              title="youtube"
+              className="youtube"
+              src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        </> : <>
+          <h1>
+            Press button to logout from NEAR Wallet
+          </h1>
+          <div className="logout-wrapper" style={{background: `url(${buttonImage})`}} onClick={logOut}>
+            <div className="logout-text">Log out</div>
+          </div>
+        </>}
       </header>
     </div>
   );
